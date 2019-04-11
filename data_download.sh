94209433982e8970ed5eca83395580c9dd0b853f
tanakat <ttanakavv.gmail.com>
2019-04-11 15:32:02 +0900

    suppress data_download.sh

--- data_download.sh
+++ data_download.sh
@@ -8,5 +8,5 @@ fi
 DATA_DIR=${1%/}
 
 mkdir -p $DATA_DIR
-wget http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz -O $DATA_DIR.tar.gz
-pv $DATA_DIR.tar.gz | tar xzf - -C $DATA_DIR
+wget -q http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz -O $DATA_DIR.tar.gz
+tar xzf $DATA_DIR.tar.gz -C $DATA_DIR
