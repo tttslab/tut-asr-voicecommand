c41cd09e13a8b9166eed84db46e1527ed044dc17
tanakat <ttanakavv.gmail.com>
2019-04-11 17:07:52 +0900

    add progress comment

--- data_download.sh
+++ data_download.sh
@@ -8,5 +8,7 @@ fi
 DATA_DIR=${1%/}
 
 mkdir -p $DATA_DIR
+echo 'start data downloading'
 wget -q http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz -O $DATA_DIR.tar.gz
+echo 'start expanding'
 tar xzf $DATA_DIR.tar.gz -C $DATA_DIR
