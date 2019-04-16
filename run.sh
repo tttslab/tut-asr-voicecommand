a7328c06adcdffd4669c45e2fbfa7ed134c0cc4f
Tomohiro Tanaka <tanaka.t.bn@m.titech.ac.jp>
2019-04-16 18:21:30 +0900

    add comment of other training set usage

--- run.sh
+++ run.sh
@@ -8,7 +8,12 @@ date
 ./prep.sh $DATA_ROOT >& prep.log
 echo 'start training'
 date
-./train.sh $MODEL_DIR $DATA_ROOT train.txt >& train.log
+./train.sh $MODEL_DIR $DATA_ROOT train2.txt >& train.log
+#./train.sh $MODEL_DIR $DATA_ROOT train20.txt >& train.log
+#./train.sh $MODEL_DIR $DATA_ROOT train40.txt >& train.log
+#./train.sh $MODEL_DIR $DATA_ROOT train60.txt >& train.log
+#./train.sh $MODEL_DIR $DATA_ROOT train80.txt >& train.log
+#./train.sh $MODEL_DIR $DATA_ROOT train.txt >& train.log
 echo 'start evaluating'
 date
 ./eval.sh $MODEL_DIR $DATA_ROOT eval.txt >& eval.log
