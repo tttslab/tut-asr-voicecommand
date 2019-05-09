7d360260ed939e571e484e54e652077cc765c55e
tanakat <tanaka.t.bn@m.titech.ac.jp>
2019-05-09 15:38:25 +0900

    fix some bug-like code and add debug output.

--- runtsubame.sh
+++ runtsubame.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-export PATH="/gs/hs0/tga-egliteracy/egs/e2e-asr/miniconda3/bin:$PATH"  ## Use miniconda virtual env
 #$ -cwd                      ## Execute a job in current directory
 #$ -l q_node=1               ## Use number of node
 #$ -l h_rt=00:10:00          ## Running job time
 
+echo "start runtsubame.sh"
+/usr/bin/env
 . /etc/profile.d/modules.sh  ## Initialize module commands
 module load cuda/9.0.176     ## Use GPU
 module load intel
@@ -12,5 +13,15 @@ module load cudnn/7.1
 module load nccl/2.2.13
 module load openmpi/2.1.2-pgi2018
 
+echo "finish load module"
+export PATH="/gs/hs0/tga-egliteracy/egs/e2e-asr/miniconda3/bin:$PATH"  ## Use miniconda virtual env
+
+/usr/bin/env
 source activate asr_exp      ## Activate virtual env
+
+echo "activated asr_exp environment"
+/usr/bin/env
+export PYTHONPATH=     ##remove PYTHONPATH if it have some path
+
+echo "run ./run.sh"
 ./run.sh
